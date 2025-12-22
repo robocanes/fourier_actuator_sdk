995aa8e70353b54bbcd9bcd2dfde681981b074ed
huigang39 <huigang39@outlook.com>
2025-12-22 14:19:56 +0800

    example适配Windows平台
    1. 修改example中的时间戳获取方式以兼容Windows平台

--- Linux-x86_64/cpp/example/multi_fsa.cpp
+++ Linux-x86_64/cpp/example/multi_fsa.cpp
@@ -1,39 +1,28 @@
 #include "fsav2.h"
 
+#include <chrono>
 #include <cstdio>
-#include <ctime>
 
 #include <thread>
 #include <vector>
-
-#define NANOSEC_PER_SEC (1000000000) // 10^9
-static uint64_t
-get_mono_timestamp_ns(void) {
-#ifdef __linux__
-  struct timespec ts;
-  clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
-  return (uint64_t)ts.tv_sec * NANOSEC_PER_SEC + ts.tv_nsec;
-#elif defined(_WIN32)
-// TODO: Windows
-#endif
-}
 
 using namespace AC2;
 
 #define FSA_NUM 7
 
-int
-main() {
-  FSA         fsas[FSA_NUM];
+int main() {
+  FSA fsas[FSA_NUM];
   const char *ip[FSA_NUM] = {
-    "192.168.137.101", "192.168.137.101", "192.168.137.101", "192.168.137.101",
-    "192.168.137.101", "192.168.137.101", "192.168.137.101",
+      "192.168.137.101", "192.168.137.101", "192.168.137.101",
+      "192.168.137.101", "192.168.137.101", "192.168.137.101",
+      "192.168.137.101",
   };
 
   for (size_t i = 0; i < FSA_NUM; i++) {
     FSA::ret_e ret = fsas[i].Init(ip[i], FSA::net_recv_mode_e::YIELD_WAIT);
     if (ret != FSA::ret_e::SUCCESS) {
-      printf("FSA(%s): init failed with code: %d\n", ip[i], static_cast<int>(ret));
+      printf("FSA(%s): init failed with code: %d\n", ip[i],
+             static_cast<int>(ret));
       return -1;
     }
   }
@@ -43,24 +32,25 @@ main() {
     threads.emplace_back([&, i]() {
       FSA::pvctte_t rx_pvctte{};
       while (true) {
-        uint64_t start = get_mono_timestamp_ns();
+        auto start = std::chrono::steady_clock::now();
 
         FSA::ret_e ret = fsas[i].GetPVCTTe(rx_pvctte);
         if (ret != FSA::ret_e::SUCCESS) {
-          printf("FSA(%s): GetPVCTTe failed with code: %d\n", ip[i], static_cast<int>(ret));
+          printf("FSA(%s): GetPVCTTe failed with code: %d\n", ip[i],
+                 static_cast<int>(ret));
         }
 
-        uint64_t end = get_mono_timestamp_ns();
+        auto end = std::chrono::steady_clock::now();
 
-        printf("FSA(%s): elapsed time: %ld us\n", ip[i], (end - start) / 1000);
+        printf(
+            "FSA(%s): elapsed time: %ld us\n", ip[i],
+            std::chrono::duration_cast<std::chrono::microseconds>(end - start)
+                .count());
 
-        printf("FSA(%s): pos: %.4lf, vel: %.4lf, cur: %.4lf, tor: %.4lf, tor_e: %.4lf\n",
-               ip[i],
-               rx_pvctte.pos,
-               rx_pvctte.vel,
-               rx_pvctte.cur,
-               rx_pvctte.tor,
-               rx_pvctte.tor_e);
+        printf("FSA(%s): pos: %.4lf, vel: %.4lf, cur: %.4lf, tor: %.4lf, "
+               "tor_e: %.4lf\n",
+               ip[i], rx_pvctte.pos, rx_pvctte.vel, rx_pvctte.cur,
+               rx_pvctte.tor, rx_pvctte.tor_e);
       }
     });
   }
--- Windows-x86_64/cpp/example/get_temp_vbus.cpp
+++ Windows-x86_64/cpp/example/get_temp_vbus.cpp
@@ -1,25 +1,12 @@
 #include "fsav2.h"
 
+#include <chrono>
 #include <cstdio>
-#include <ctime>
-
-#define NANOSEC_PER_SEC (1000000000) // 10^9
-static uint64_t
-get_mono_timestamp_ns(void) {
-#ifdef __linux__
-  struct timespec ts;
-  clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
-  return (uint64_t)ts.tv_sec * NANOSEC_PER_SEC + ts.tv_nsec;
-#elif defined(_WIN32)
-// TODO: Windows
-#endif
-}
 
 using namespace AC2;
 
-int
-main() {
-  FSA         fsa;
+int main() {
+  FSA fsa;
   const char *ip = "192.168.137.101";
 
   FSA::ret_e ret = fsa.Init(ip, FSA::net_recv_mode_e::YIELD_WAIT);
@@ -31,21 +18,21 @@ main() {
   FSA::temp_vbus_t rx_temp_vbus{};
 
   while (true) {
-    uint64_t start = get_mono_timestamp_ns();
+    auto start = std::chrono::steady_clock::now();
 
     ret = fsa.GetTempVbus(rx_temp_vbus);
     if (ret != FSA::ret_e::SUCCESS)
-      printf("FSA(%s): Get Temp and Vbus failed with code: %d\n", ip, static_cast<int>(ret));
+      printf("FSA(%s): Get Temp and Vbus failed with code: %d\n", ip,
+             static_cast<int>(ret));
 
-    uint64_t end = get_mono_timestamp_ns();
+    auto end = std::chrono::steady_clock::now();
 
-    printf("FSA(%s): elapsed time: %ld us\n", ip, (end - start) / 1000);
+    printf("FSA(%s): elapsed time: %ld us\n", ip,
+           std::chrono::duration_cast<std::chrono::microseconds>(end - start)
+               .count());
 
-    printf("FSA(%s): mos_temp: %.4lf, coil_temp: %.4lf, vbus: %.4lf\n",
-           ip,
-           rx_temp_vbus.mos,
-           rx_temp_vbus.coil,
-           rx_temp_vbus.vbus);
+    printf("FSA(%s): mos_temp: %.4lf, coil_temp: %.4lf, vbus: %.4lf\n", ip,
+           rx_temp_vbus.mos, rx_temp_vbus.coil, rx_temp_vbus.vbus);
   }
 
   return 0;
